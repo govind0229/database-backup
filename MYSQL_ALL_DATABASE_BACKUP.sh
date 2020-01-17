408f50f750ff9eb409500ddfac82f1df9371f6b6
Govind Sharma <30208047+govind0229@users.noreply.github.com>
2020-01-17 11:21:57 +0530

    Create MYSQL_ALL_DATABASE_BACKUP.sh

---
+++ MYSQL_ALL_DATABASE_BACKUP.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# Shell script to backup MySQL database
+# Created By Govind Sharma
+set -e
+
+MyUSER="root"	# DB_USERNAME
+MyPASS="eltsen"	# DB_PASSWORD
+MyHOST="localhost"	# DB_HOSTNAME
+DEST="/home/govind"
+DAYS=10
+MYSQL="$(which mysql)"
+MYSQLDUMP="$(which mysqldump)"
+GZIP="$(which gzip)"
+YER="$(date +"%Y")"
+MON="$(date +"%m")"
+NOW="$(date +"%d")"
+MBD="$DEST/$YER/$MON/$NOW/"
+install -C -d $MBD
+chmod 777 $MBD;
+
+# Skip database list
+SKIP="information_schema mysql performance_schema"
+
+DBL="$($MYSQL -h $MyHOST -u $MyUSER -p$MyPASS -Bse 'show databases')"
+
+for db in $DBL
+do
+    skipdb=-1
+        if [ "$SKIP" != "" ];
+            then
+                for i in $SKIP
+                    do
+                        [ "$db" == "$i" ] && skipdb=1 || :
+                    done
+        	fi
+            	if [[ -f "$MBD/$db.sql.gz" ]]; then
+                	echo "$db database backup exist"
+        	else
+            	if [ "$skipdb" == "-1" ] ; then
+                	echo "$db database Backup start";
+	                FILE="$MBD/$db.sql"
+	                $MYSQLDUMP -h $MyHOST -u $MyUSER -p$MyPASS $db > $FILE
+	                $GZIP $FILE
+            fi
+        fi
+done
+
+# Remove old files
+cd $DEST/$YER/$MON/
+find $NOW -mtime +$DAYS -exec rm -f {} \;
